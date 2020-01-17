b07e17cf6cd2712ff485c85d44943af2ad710b3c
jasekk99 <59716766+jasekk99@users.noreply.github.com>
2020-01-17 11:16:50 +0100

    Initial commit

---
+++ Formular.php
@@ -0,0 +1,9 @@
+<?php
+    echo '<u> Deine Angaben: </u> <br>';
+    echo 'Dein Name: ' .$_POST['vorname'] .'<br>';
+    echo 'Dein Nachname: ' .$_POST['nachname'] .'<br>';
+    echo 'Land: ' .$_POST['land'] .'<br>';
+    echo 'Subjekt:' .$_POST['subjekt'] .'<br>';
+    echo 'Deine Rückmeldung: ' .$_POST['nachricht'] .'<br>';
+    echo 'Deine E-Mail: ' .$_POST['mail'];
+    ?>
---
+++ test.js
@@ -0,0 +1,5 @@
+const instance = new TypeIt('span', {
+    strings: "Fortnite: Crank the 90"
+    speed: 100
+  }).go();
+  
